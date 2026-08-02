1e626e821429ea71cb0d2763fa2c91b5147d0d88
jan146 <anurjan@gmail.com>
2026-08-02 22:29:42 +0200

    SSH agent

--- openssh.nix
+++ openssh.nix
@@ -2,4 +2,6 @@
 {
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
+  # Enable agent that remembers private keys
+  programs.ssh.startAgent = true;
 }
--- sudo.nix
+++ sudo.nix
@@ -1,4 +1,9 @@
 { ... }:
 {
+  # Wheel users don't need to enter password
   security.sudo.wheelNeedsPassword = false;
+  # Preserve ssh-agent session
+  security.sudo.extraConfig = ''
+    Defaults env_keep += "SSH_AUTH_SOCK"
+  '';
 }
