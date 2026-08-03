d25ee23b7d16d3b967245901c63ac42cbbd178f4
jan146 <anurjan@gmail.com>
2026-08-03 21:08:14 +0200

    Add nix-index-database

--- flake.nix
+++ flake.nix
@@ -17,14 +17,21 @@
       url = "github:wbthomason/packer.nvim";
       flake = false;
     };
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = inputs@{ self, nixpkgs, hjem, ... }: {
+  outputs = inputs@{ self, nixpkgs, hjem, nix-index-database, ... }: {
     nixosConfigurations.venice = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
       modules = [
         ./configuration.nix
         inputs.hjem.nixosModules.default
+        nix-index-database.nixosModules.default
+        # optional to also wrap and install comma
+        # { programs.nix-index-database.comma.enable = true; }
       ];
     };
   };
