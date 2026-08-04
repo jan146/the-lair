92de648f6be3306c41e08172561e2e387ac2f3ed
jan146 <anurjan@gmail.com>
2026-08-04 22:01:55 +0200

    Move docmost nginx config

--- docmost.nix
+++ docmost.nix
@@ -66,4 +66,14 @@ in
       ];
     };
   };
+  services.nginx = {
+    virtualHosts."docmost.${config.domainName}" =  {
+      enableACME = true;
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:3000";
+        proxyWebsockets = true;
+      };
+    };
+  };
 }
