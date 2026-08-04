5c9ebff59e9005c2e6ba29f72981b87e04e73ae8
jan146 <anurjan@gmail.com>
2026-08-04 19:15:04 +0200

    Murmur: use ssl cert

--- murmur.nix
+++ murmur.nix
@@ -1,8 +1,16 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
+let
+  sslCertDir = config.security.acme.certs.${config.domainName}.directory;
+in
 {
   services.murmur = {
     enable = true;
     port = 64738;
+    tls = {
+      caPath = "${sslCertDir}/chain.pem";
+      certPath = "${sslCertDir}/fullchain.pem";
+      keyPath = "${sslCertDir}/key.pem";
+    };
   };
   environment.systemPackages = with pkgs; [
     mumble
