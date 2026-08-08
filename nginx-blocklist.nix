97b669d77f769a7047b6e9296906e3db854d3832
jan146 <anurjan@gmail.com>
2026-08-08 19:38:14 +0200

    Make robots.txt available to clankers, but reject all other locations

--- nginx-blocklist.nix
+++ nginx-blocklist.nix
@@ -1,9 +1,19 @@
-vhost@{ extraConfig ? "", ... }:
+vhost@{ locations ? {},... }:
+let
+  addRobotsLocation = locations: locations // {
+    "/robots.txt" = locations."/robots.txt" or {
+      return = ''200 "User-agent: *\nDisallow: /\n"'';
+    };
+  };
+  addExtraConfig = locations: (builtins.mapAttrs(location: conf: conf // {
+    extraConfig = (conf.extraConfig or "") + ''
+      # Block AI crawlers that announce themselves in the HTTP user agent.
+      if ($is_clanker) {
+          return 403;
+      }
+    '';
+  })) locations;
+in
 vhost // {
-  extraConfig = (extraConfig) + ''
-    # Block AI crawlers that announce themselves in the HTTP user agent.
-    if ($is_clanker) {
-        return 403;
-    }
-  '';
+  locations = addRobotsLocation (addExtraConfig locations);
 }
