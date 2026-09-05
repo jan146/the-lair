cca9f1853be5629d5841a6191422db4ba4ee768a
jan146 <anurjan@gmail.com>
2026-09-05 17:32:49 +0200

    Disable display server to free up resources

--- desktop.nix
+++ desktop.nix
@@ -5,7 +5,7 @@
   services.xserver.enable = false;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.enable = false;
   services.desktopManager.plasma6.enable = true;
 
   # Enable CUPS to print documents.
